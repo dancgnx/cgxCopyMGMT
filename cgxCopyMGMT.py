34c0355ec21a468789be4ffc16a84e107805884c
Dan SG <dan@cloudgenix.com>
2019-07-23 21:26:42 -0700

    add comments

--- cgxCopyMGMT.py
+++ cgxCopyMGMT.py
@@ -8,6 +8,9 @@ import logging
 import csv
 
 def getElementByName(cgx,name):
+    """
+    get Element json object by using the element name
+    """
     res = cgx.get.elements()
     if not res.cgx_status:
         print(res.cgx_content)
@@ -17,6 +20,9 @@ def getElementByName(cgx,name):
             return item
     return None
 def getInterfaceByName(cgx,site_id,element_id,name):
+    """
+    get Interface json object by using the interface name
+    """
     res = cgx.get.interfaces(site_id=site_id,element_id=element_id)
     if not res.cgx_status:
         print(res.cgx_content)
@@ -27,6 +33,9 @@ def getInterfaceByName(cgx,site_id,element_id,name):
     return None
 
 def getDeviceManagementExtensionByInterface(cgx,site_id,element_id,interface_id):
+    """
+    get the existing config for the interface
+    """
     res = cgx.get.element_extensions(site_id=site_id,element_id=element_id)
     if not res.cgx_status:
         print(res.cgx_content)
@@ -37,6 +46,9 @@ def getDeviceManagementExtensionByInterface(cgx,site_id,element_id,interface_id)
     return None
 
 def pasteDeviceManagementAccessToInterface(cgx,s_ext,t_elementname,t_interfacename):
+    """
+    apply source extension json object to the interface
+    """
 
     log.info(f"Copy to interface {t_interfacename} of element {t_elementname}")
 
@@ -61,6 +73,8 @@ def pasteDeviceManagementAccessToInterface(cgx,s_ext,t_elementname,t_interfacena
         if not res:
             log.error(f"----- Can't delete existing extension from interface {t_interfacename}. Error {res.cgx_content}")
             sys.exit(-1)
+    
+    # build the extension structure
     t_ext = {     
         "name": s_ext["name"],
         "namespace": "devicemanagement/interface",
@@ -68,6 +82,8 @@ def pasteDeviceManagementAccessToInterface(cgx,s_ext,t_elementname,t_interfacena
         "disabled": s_ext["disabled"],
         "conf": s_ext["conf"]
         }    
+    
+    # add the extension to the interfaces
     res = cgx.post.element_extensions(t_siteid, t_eid,t_ext)
     if not res:
         log.error(f"----- Couln't add extension. Error {res.cgx_content}")
@@ -95,7 +111,7 @@ if __name__ == "__main__":
         sys.exit(-1)
     s_interfaceid = s_interface["id"]
 
-    #get extension
+    # get existing device access policy for the source interface
     s_ext = getDeviceManagementExtensionByInterface(cgx,s_siteid,s_eid,s_interfaceid)
     if not s_ext:
         log.error("Source interface has no configurations")
@@ -104,16 +120,19 @@ if __name__ == "__main__":
     if args["list"]:
         jd(s_ext)
     elif args["t_element"] and args["t_interface"]:
-        # update a single interface
+        # update a single target
         pasteDeviceManagementAccessToInterface(cgx,s_ext,args["t_element"],args["t_interface"])
     elif args["interface_file"]:
         # read targets from file
         with open(args["interface_file"]) as f:
             interfaces = csv.reader(f)
             for interface in interfaces:
+                # check if there are two and only two items in each line
                 if len(interface) != 2:
                     log.error(f"Invalid line. Should have 'element name,interfacename' but got {','.join(interface)}")
                     sys.exit(-1)
+                # update a single interface
                 pasteDeviceManagementAccessToInterface(cgx,s_ext,interface[0],interface[1])
     else:
+        # if we reached here, then the wrong combination of parameters where used
         print ("ERROR: You must specify target element and target itnerface or a file with list of targets")
